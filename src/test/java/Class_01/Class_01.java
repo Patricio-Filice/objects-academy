74484afaf441d0f5e84bd36ece367c74a393cb63
pfilice <patricio.filice@gmail.com>
2019-05-05 02:45:01 -0300

    Init

---
+++ src/test/java/Class_01/Class_01.java
@@ -0,0 +1,14 @@
+package Class_01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Class_01 {
+
+    @Test
+    public void helloLogic() {
+        int numero = 1;
+        assertEquals(1, numero);
+    }
+}
